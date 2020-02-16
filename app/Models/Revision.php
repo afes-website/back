455f961f3fb4022cc10647ae451376c03b4f18d2
ibuki2003 <ibuki426.sp@gmail.com>
2020-02-16 16:38:02 +0900

    add model Revision

---
+++ app/Models/Revision.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Revision extends Model
+{
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id', 'title', 'article_id', 'user_id', 'timestamp', 'content', 'status'
+    ];
+
+    protected $primaryKey = 'id';
+
+    protected $keyType = 'int';
+
+    public $incrementing = false;
+
+    public $timestamps = false;
+
+}

9e1432b0bfb86cac7307a16709e6d003806a889b
ibuki2003 <ibuki426.sp@gmail.com>
2020-02-16 16:40:05 +0900

    add get revision route

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -2,7 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Revision;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class BlogController extends Controller {
+    public function get_revision($id){
+        $revision = Revision::find($id);
+
+        if(!$revision)
+            throw new HttpException(404);
+
+        $revision -> timestamp = date(DATE_ISO8601, $revision -> timestamp);
+        $article_info = $revision;
+        return response()->json($article_info, 200);
+    }
 }

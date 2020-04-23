31c62b044d45539a4af80dc3c66f082792c19028
Takeno-hito <18237819+Takeno-hito@users.noreply.github.com>
2020-04-23 19:45:57 +0900

    feat: add validate

--- app/Http/Controllers/OGImageController.php
+++ app/Http/Controllers/OGImageController.php
@@ -2,17 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
+use Illuminate\Http\Request;
 class OGImageController extends Controller {
     private function generate($title, $author = NULL, $category = NULL){
         return;
     }
 
     public function getImage(Request $request){
+        $this->validate($request, [
+            'title' => ['required', 'string'],
+        ]);
     }
 
     public function getArticleImage($id){
+        $article = Article::find($id);
+        if(!$article) abort(404);
     }
 
     public function getPreview(Request $request){
+        $this->validate($request, [
+            'title' => ['required', 'string'],
+            'author'=> ['string'],
+            'category' => ['string'],
+        ]);
     }
 }

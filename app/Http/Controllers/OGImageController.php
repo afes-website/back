cdf8ba35f3c8d457b242aaf202d110cc8704a47c
Takeno-hito <18237819+Takeno-hito@users.noreply.github.com>
2020-04-23 19:42:36 +0900

    feat: add OGImage Controller

---
+++ app/Http/Controllers/OGImageController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class OGImageController extends Controller {
+    private function generate($title, $author = NULL, $category = NULL){
+        return;
+    }
+
+    public function getImage(Request $request){
+    }
+
+    public function getArticleImage($id){
+    }
+
+    public function getPreview(Request $request){
+    }
+}

ef24a0f03ee1cbc90eeafc8103742c81525a9a39
Takeno-hito <18237819+Takeno-hito@users.noreply.github.com>
2020-04-23 21:39:26 +0900

    feat: make flow by feeling

--- app/Http/Controllers/OGImageController.php
+++ app/Http/Controllers/OGImageController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Article;
 use Illuminate\Http\Request;
+
 class OGImageController extends Controller {
     private function generate($title, $author = NULL, $category = NULL){
         return;
@@ -13,11 +14,15 @@ class OGImageController extends Controller {
         $this->validate($request, [
             'title' => ['required', 'string'],
         ]);
+
+        $this->generate($request->input('title'));
     }
 
     public function getArticleImage($id){
         $article = Article::find($id);
         if(!$article) abort(404);
+
+        $this->generate($article->title, $article->revision->user->name, $article->category);
     }
 
     public function getPreview(Request $request){
@@ -26,5 +31,6 @@ class OGImageController extends Controller {
             'author'=> ['string'],
             'category' => ['string'],
         ]);
+        $this->generate($request->input('title'), $request->input('author'), $request->input('category'));
     }
 }

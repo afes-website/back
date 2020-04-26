00697f58da72ed9cda09a6a9deabe92f220d1989
ibuki2003 <ibuki426.sp@gmail.com>
2020-04-27 02:08:18 +0900

    test: split tests for methods into inidividual functions

--- tests/OGImageTest.php
+++ tests/OGImageTest.php
@@ -11,11 +11,13 @@ class OGImageTest extends TestCase {
         $this->assertEquals($mime_type, $content_type_hdrs[0]);
     }
 
-    public function test_ok() {
+    public function test_normal() {
         $this->call('GET', '/ogimage', ['title' => Str::random(10)]);
         $this->assertResponseOk();
         $this->assertMimeTypeEqualsTo('image/png');
+    }
 
+    public function test_article() {
         $article_id = Str::random(32);
         $writer_user = WriterAuthJwt::get_token($this);
         $revision = factory(Revision::class)->create([
@@ -31,19 +33,25 @@ class OGImageTest extends TestCase {
         $this->get("/ogimage/articles/$article_id");
         $this->assertResponseOk();
         $this->assertMimeTypeEqualsTo('image/png');
+    }
 
+    public function test_preview() {
         $this->call('GET', '/ogimage/preview', ['title' => Str::random(10)]);
         $this->assertResponseOk();
         $this->assertMimeTypeEqualsTo('image/png');
     }
-    public function test_no_param() {
+
+    public function test_normal_invalid() {
         $this->get('/ogimage');
         $this->assertResponseStatus(400);
+    }
 
+    public function test_preview_invalid() {
         $this->get('/ogimage/preview');
         $this->assertResponseStatus(400);
     }
-    public function test_articles_invalid_id() {
+
+    public function test_article_invalid() {
         $id = Str::random(40);
         $this->get("/ogimage/articles/$id");
         $this->assertResponseStatus(404);

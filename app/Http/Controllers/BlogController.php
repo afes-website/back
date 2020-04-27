7fa3a3cefa0dde108ecdd92744f81a2ec8a754fb
Takeno-hito <18237819+Takeno-hito@users.noreply.github.com>
2020-04-27 22:36:43 +0900

    fix: add Last-Modified

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers;
 
 use Laravel\Lumen\Routing\Controller as BaseController;
+use Carbon\Carbon;
 
 class BlogController extends BaseController
 {
     public function category_index(){
-        return response()->json(config('blog.categories'));
+        $dt = Carbon::createFromFormat('Y-m-d H:i:s.u', '2020-04-27 13:33:00.000000');
+        return response()->json(config('blog.categories'))
+            ->header('Last-Modified', $dt->toRfc7231String());
+
     }
 }

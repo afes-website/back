e8a6bd9877bc44eb1f8b4bc33290d8fde1ef58b6
Takeno-hito <18237819+Takeno-hito@users.noreply.github.com>
2020-08-24 16:12:31 +0900

    feat: create new users table

---
+++ database/migrations/2020_08_24_055926_create_new_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNewUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->string('id', 16);
+            $table->primary('id');
+            $table->string('name');
+            $table->string('password');
+
+            $table->boolean("perm_admin");
+            $table->boolean("perm_blogAdmin");
+            $table->boolean("perm_blogWriter");
+            $table->boolean("perm_exhibition");
+            $table->boolean("perm_general");
+            $table->boolean("perm_reservation");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user');
+    }
+}

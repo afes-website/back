7575738f13952941953e355e286fa1fae9d7908c
Takeno-hito <18237819+Takeno-hito@users.noreply.github.com>
2020-08-25 11:31:22 +0900

    feat: create reservations table migration

---
+++ database/migrations/2020_08_25_021722_create_reservations_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReservationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->string('id');
+            $table->primary('id');
+            $table->string('email');
+            $table->integer('people_count');
+            $table->string('time_id');
+            $table->string('name');
+            $table->string('address');
+            $table->string('cellphone');
+            $table->string('guest_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservations');
+    }
+}

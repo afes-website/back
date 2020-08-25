2ef93f0a4c6bea3c05120a7d6657307d3d94c222
Takeno-hito <18237819+Takeno-hito@users.noreply.github.com>
2020-08-25 11:49:50 +0900

    feat: create guests table migration

---
+++ database/migrations/2020_08_25_023544_create_guests_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGuestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('guests', function (Blueprint $table) {
+            $table->string('id');
+            $table->primary('id');
+            $table->timestamp('entered_at')->useCurrent();
+            $table->timestamp('exit_scheduled_at');
+            $table->timestamp('exited_at')->nullable();
+            $table->string('reservation_id');
+            $table->string('exh_id')->nullable();
+            $table->string('term_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('guests');
+    }
+}

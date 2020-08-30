06b9c1c293acba020b903ad380cd706424840706
Takeno-hito <18237819+Takeno-hito@users.noreply.github.com>
2020-08-30 20:53:27 +0900

    fix: to response if query has unnessesary content

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -2,18 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Resources\RevisionResource;
 use App\Models\Reservation;
-use App\Models\Revision;
-use App\Models\WriterUser;
 use Illuminate\Http\Request;
-use App\SlackNotify;
 use \Illuminate\Support\Str;
 
 
 class ReservationController extends Controller {
     public function create(Request $request) {
-        $this->validate($request, [
+        $body = $this->validate($request, [
             'email' => ['required', 'string', 'email:rfc,dns'],
             'term_id' => ['required', 'string'],
             'people_count' => ['required', 'integer', 'gte:1'],
@@ -24,12 +20,12 @@ class ReservationController extends Controller {
 
         $reservation_id = 'R_'.Str::random(10);
         while(true){
-            if(!Revision::where('article_id', $reservation_id)->exists()) break;
+            if(!Reservation::where('id', $reservation_id)->exists()) break;
             $reservation_id = 'R_'.Str::random(10);
         }
 
         $reservation = Reservation::create(
-            array_merge($request->all(), ['id' => $reservation_id])
+            array_merge($body, ['id' => $reservation_id])
         );
 
         return response($reservation,201);

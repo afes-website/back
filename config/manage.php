dac29c822ffabb26c44ac60abe94770cacd55efd
Takeno-hito <18237819+Takeno-hito@users.noreply.github.com>
2020-09-06 16:53:31 +0900

    feat: craete manage config and write color

---
+++ config/manage.php
@@ -0,0 +1,48 @@
+<?php
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Color List
+    |--------------------------------------------------------------------------
+    |
+    | Used for GuestController
+    | This restrict the WristBand Prefix
+    */
+
+    'colors' => [
+        'blue' => [
+            'prefix' => 'GB'
+        ],
+        'red' => [
+            'prefix' => 'GR'
+        ],
+        'yellow' => [
+            'prefix' => 'GY'
+        ],
+        'violet' => [
+            'prefix' => 'GV'
+        ],
+        'orange' => [
+            'prefix' => 'GO'
+        ],
+        'green' => [
+            'prefix' => 'GG'
+        ],
+        'white' => [
+            'prefix' => 'GW'
+        ],
+        'gray' => [
+            'prefix' => 'GS'
+        ],
+        'test_blue' => [
+            'prefix' => 'TB'
+        ],
+        'test_red' => [
+            'prefix' => 'TR'
+        ],
+        'test_yellow' => [
+            'prefix' => 'TY'
+        ]
+    ]
+];

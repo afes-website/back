0cd612d30cc91d533db9cf332ffa475da8ceb0b3
Takeno-hito <18237819+Takeno-hito@users.noreply.github.com>
2020-09-28 21:36:08 +0900

    feat: add exhibition resource

---
+++ app/Http/Resources/ExhibitionResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\Resource;
+
+class ExhibitionResource extends Resource
+{
+    /**
+     * リソースを配列へ変換する
+     *
+     * @param  \Illuminate\Http\Request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'thumbnail_image_id' => $this->thumbnail_image_id,
+            'content' => $this->draft->content,
+            'updated_at' => $this->updated_at
+        ];
+        // id: string;
+        //  name: string;
+        //  thumbnail_image_id: string;
+        //  content: string;
+        //  updated_at: string;
+    }
+}
+

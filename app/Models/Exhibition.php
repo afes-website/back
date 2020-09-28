9ee12f534e689266e6d4ca64468231edfb71c1e0
Takeno-hito <18237819+Takeno-hito@users.noreply.github.com>
2020-09-28 20:50:09 +0900

    feat: add exhibition model

---
+++ app/Models/Exhibition.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Exhibition extends Model
+{
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id', 'name', 'thumbnail_image_id', 'draft_id', 'updated_at'
+    ];
+
+    protected $primaryKey = 'id';
+
+    protected $keyType = 'string';
+
+    public $incrementing = false;
+
+    public $timestamps = true;
+
+    const UPDATED_AT = 'updated_at';
+
+    public function draft() {
+        return $this->belongsTo('\App\Models\Draft');
+    }
+
+}

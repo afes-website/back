ea99e05d62474a32c315cfea74fc95c5b3649a2a
subaru <s@su8ru.dev>
2020-11-05 23:14:27 +0900

    fix: update Last-Modified

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -8,7 +8,7 @@ use Carbon\Carbon;
 class BlogController extends BaseController
 {
     public function category_index(){
-        $dt = Carbon::createFromFormat('Y-m-d H:i:s.u', '2020-04-28 13:33:00.000000');
+        $dt = Carbon::createFromFormat('Y-m-d H:i T', '2020-11-05 23:00 +0900');
         return response()->json(config('blog.categories'))
             ->header('Last-Modified', $dt->toRfc7231String())
             ->header('Cache-Control', 'public, max-age=604800'); // 7 Days

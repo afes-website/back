0655d669762d26143b2e8521a1b115d7cdfad8fe
subaru <s@su8ru.dev>
2020-11-05 23:15:21 +0900

    fix: remove Cache-Control

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -10,7 +10,6 @@ class BlogController extends BaseController
     public function category_index(){
         $dt = Carbon::createFromFormat('Y-m-d H:i T', '2020-11-05 23:00 +0900');
         return response()->json(config('blog.categories'))
-            ->header('Last-Modified', $dt->toRfc7231String())
-            ->header('Cache-Control', 'public, max-age=604800'); // 7 Days
+            ->header('Last-Modified', $dt->toRfc7231String());
     }
 }

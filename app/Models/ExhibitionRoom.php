9c6b3124aee5a10832ab2bf1de14a63942aea44a
Takeno-hito <18237819+Takeno-hito@users.noreply.github.com>
2021-02-18 10:11:45 +0900

    feat: exhibition room model/migration

---
+++ app/Models/ExhibitionRoom.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Database\Eloquent\Model;
+use Laravel\Lumen\Auth\Authorizable;
+
+class ExhibitionRoom extends Model implements AuthenticatableContract, AuthorizableContract
+{
+    use Authenticatable, Authorizable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+
+    protected $fillable = [
+        'id', 'room_id', 'capacity', 'guest_count', 'updated_at'
+    ];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [];
+
+    protected $primaryKey = 'id';
+
+    protected $keyType = 'string';
+
+    public $timestamps = true;
+
+    const CREATED_AT = null;
+
+    const UPDATED_AT = 'updated_at';
+}

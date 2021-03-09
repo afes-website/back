c96b8cc6640913e7a95660eb4fa6c99e8e88cdd3
Takeno-hito <18237819+Takeno-hito@users.noreply.github.com>
2021-03-09 23:11:08 +0900

    test: create InvalidGuestCodeCheck

--- tests/onsite/General/EntranceTest.php
+++ tests/onsite/General/EntranceTest.php
@@ -20,4 +20,34 @@ class EntranceTest extends TestCase {
         );
         $this->assertResponseOk();
     }
+
+    public function testInvalidGuestCode() {
+        $invalid_codes = [];
+        $count = 5;
+
+        $user = factory(User::class, 'general')->create();
+        $term = factory(Term::class)->create();
+        $reservation = factory(Reservation::class)->create([
+            'term_id' => $term->id
+        ]);
+
+        for ($i = 0; $i < $count; ++$i) {
+            do {
+                $prefix = rand(1, 10);
+                $id = rand(1, 10);
+            } while ($prefix == 2 && $id == 5);
+            $invalid_codes[] = Str::random($prefix).'-'.Str::random($id);
+        }
+
+        foreach ($invalid_codes as $invalid_code) {
+            $this->actingAs($user)->post(
+                '/onsite/general/enter',
+                ['guest_id' => $invalid_code, 'reservation_id' => $reservation->id]
+            );
+            $this->assertResponseStatus(400);
+            $this->receiveJson();
+            $code = json_decode($this->response->getContent())->error_code;
+            $this->assertEquals($code, 'INVALID_WRISTBAND_CODE');
+        }
+    }
 }

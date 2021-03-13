b953bf5005344ab402898086108f8f8840a37644
ibuki2003 <main@fuwa.dev>
2021-03-13 19:47:15 +0900

    refactor: fix style problems

--- database/migrations/2021_03_13_173753_add_draft_comment_author_constraint.php
+++ database/migrations/2021_03_13_173753_add_draft_comment_author_constraint.php
@@ -4,15 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddDraftCommentAuthorConstraint extends Migration
-{
+class AddDraftCommentAuthorConstraint extends Migration {
+
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
+    public function up() {
         //
         Schema::table('draft_comments', function (Blueprint $table) {
             $table->foreign('author_id')->references('id')->on('users')
@@ -25,8 +24,7 @@ class AddDraftCommentAuthorConstraint extends Migration
      *
      * @return void
      */
-    public function down()
-    {
+    public function down() {
         Schema::table('draft_comments', function (Blueprint $table) {
             $table->dropForeign(['author_id']);
         });

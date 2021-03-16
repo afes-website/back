b025de9b299ba81d364ab25e7206d361d7cceafa
Takeno-hito <18237819+Takeno-hito@users.noreply.github.com>
2021-03-16 23:24:36 +0900

    test: guest/All

--- tests/onsite/General/GuestTest.php
+++ tests/onsite/General/GuestTest.php
@@ -6,6 +6,30 @@ namespace Tests;
  *
  * /:get /_id:get /_id/log:get
  */
-class GuestTest extends TestCase {
 
+use App\Models\Guest;
+use App\Models\Term;
+use App\Models\User;
+
+class GuestTest extends TestCase {
+    public function testGetAll() {
+        $term_count = 3;
+        $guest_count = 3;
+        $user = factory(User::class, 'general')->create();
+
+        for ($i = 0; $i < $term_count; $i++) {
+            $term[] = factory(Term::class)->create();
+            for ($j = 0; $j < $guest_count; $j++) {
+                $guest[] = factory(Guest::class)->create([
+                    'term_id' => $term[$i]->id
+                ]);
+            }
+        }
+
+        $this->actingAs($user)->get('/onsite/general/guest');
+        $this->assertResponseOk();
+        $this->receiveJson();
+        $res = json_decode($this->response->getContent());
+        $this->assertCount($term_count * $guest_count, $res);
+    }
 }

872f3357174a0335ac4a6c125ce9e93f897ca5d8
ibuki2003 <main@fuwa.dev>
2021-03-22 01:31:44 +0900

    feat: disallow sqlite

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,8 +3,15 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\DB;
 
 class AppServiceProvider extends ServiceProvider {
+    public function boot() {
+        if (DB::getDriverName() == 'sqlite') {
+            fputs(STDERR, "sqlite is no longer supported. use other database such as MySQL.\n");
+            exit(1);
+        }
+    }
 
     /**
      * Register any application services.
